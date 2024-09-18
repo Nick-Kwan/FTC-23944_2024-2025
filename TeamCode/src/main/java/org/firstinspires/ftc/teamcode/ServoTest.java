36953c60cff74ae834d9d2653efeed596561d547
Nick-Kwan <144179772+Nick-Kwan@users.noreply.github.com>
2024-09-18 16:55:03 -0500

    Rizz
    Rizz

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoTest
+{
+    public void runServo() {
+        //Declare servo
+        Servo edge = hardwareMap.get(Servo.class, "Release Servo");
+        edge.setPosition(.69);
+        //Adds position data of servo to control hub
+        telemetry.addData("Release Servo", edge.getPosition());
+    }
+}
