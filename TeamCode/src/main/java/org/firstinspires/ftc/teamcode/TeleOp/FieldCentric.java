2f690cc07b99222f67ff81c0b017945131fff2f7
sparky520 <jonathanfishernj@gmail.com>
2024-11-18 16:34:18 -0600

    raymac

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentric.java
@@ -51,142 +51,24 @@ public class FieldCentric extends OpMode
 
 
         if (driver.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)){
-            bot.servoClaw.setClawPosition0();
+            bot.aX.setArmPosition0();
         }
         if (driver.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER)){
-            bot.servoClaw.setClawPosition1();
+            bot.aX.setArmPosition1();
+        }
+        if (driver.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
+            bot.aX.setALPosition0();
+        }
+        if (driver.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)){
+            bot.aX.setALPosition1();
+        }
+        if (driver.wasJustPressed(GamepadKeys.Button.X)){
+            bot.aX.setARPosition0();
+        }
+        if (driver.wasJustPressed(GamepadKeys.Button.B)){
+            bot.aX.setARPosition1();
         }
 
-//        In Sub 2
-        if (operator.wasJustPressed(GamepadKeys.Button.B)){
-            bot.servoClaw.setClawPosition0();
-            bot.sr.setPosition(120);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.s.setPosition(1360);
-        }
-        //        In Sub 1
-        if (operator.wasJustPressed(GamepadKeys.Button.A)){
-            bot.servoClaw.setClawPosition0();
-            bot.sr.setPosition(120);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.s.setPosition(650);
-        }
-
-//        Fish
-        if (driver.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)){
-            bot.servoClaw.setClawPosition0();
-            try {
-                Thread.sleep(250);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.sr.setPosition(0);
-            try {
-                Thread.sleep(750);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            bot.servoClaw.setClawPosition1();
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.sr.setPosition(130);
-        }
-        //        Rest
-        if (operator.wasJustPressed(GamepadKeys.Button.Y)){
-            bot.s.setPosition(0);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.sr.setPosition(0);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.s.setPower0();
-            bot.sr.setPower0();
-        }
-        //pick up specimen
-        if (driver.wasJustPressed(GamepadKeys.Button.DPAD_UP)){
-            bot.sr.setPosition(97);
-            bot.servoClaw.setClawPosition0();
-        }
-
-        //Low Bucket
-        if (operator.wasJustPressed(GamepadKeys.Button.X)){
-            bot.sr.setPosition(550);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.s.setPosition(1500);
-        }
-        //        High Specimen
-        if (operator.wasJustPressed(GamepadKeys.Button.DPAD_UP)){
-            bot.sr.setPosition(550);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.s.setPosition(1280);
-        }
-        //       High Slam
-       /* if (operator.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)){
-            bot.sr.setPosition(450);
-
-            bot.s.setPosition(900);
-            try {
-                Thread.sleep(750);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.servoClaw.setClawPosition0();
-        }*/
-        //        Low Specimen
-        if (operator.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)){
-            bot.sr.setPosition(400);
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.s.setPosition(532);
-        }
-        if (driver.wasJustPressed(GamepadKeys.Button.Y)){
-            bot.s.setPosition(1600);
-            bot.sr.setPosition(750);
-
-        }
-        //        Low Slam
-      /*  if (operator.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)){
-            bot.sr.setPosition(240);
-
-            bot.s.setPosition(481);
-            try {
-                Thread.sleep(750);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            bot.servoClaw.setClawPosition0();
-        }
-
-       */
 
         if (driver.wasJustPressed(GamepadKeys.Button.START))
         {
