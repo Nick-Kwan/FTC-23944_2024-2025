5cf96e0a5cc03ef71f96943ad4d946d3dcbb9694
sparky520 <jonathanfishernj@gmail.com>
2024-12-28 15:41:39 -0600

    boop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Executables/Path/Specimen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Executables/Path/Specimen.java
@@ -16,13 +16,68 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Auto.Executables.ActionsFolder.BotActions;
-import org.firstinspires.ftc.teamcode.Auto.Executables.ActionsFolder.ClawActions;
-import org.firstinspires.ftc.teamcode.Auto.Executables.ActionsFolder.SlideActions;
 import org.firstinspires.ftc.teamcode.Auto.RRdrives.PinpointDrive;
 
 import java.util.Arrays;
 
+@Config
+@Autonomous(name = "specimen", group = "Autonomous")
+public class Specimen extends LinearOpMode{
+    PinpointDrive drive;
+    TelemetryPacket tel = new TelemetryPacket();
+    SequentialAction path;
+    Pose2d start = new Pose2d(8,-8,Math.toRadians(180));
+    BotActions bot;
+    boolean running;
+    VelConstraint baseVelConstraint = new MinVelConstraint(Arrays.asList(
+            new TranslationalVelConstraint(20),
+            new AngularVelConstraint(Math.PI/3)
+    ));
 
-public class Specimen {
+    public enum AutoStates{
+        followingPath, idle
+    }
+    AutoStates autoStates = AutoStates.followingPath;
+    //x is forward
+    //y is left and right, right is negative
+    @Override
+    public void runOpMode() throws InterruptedException {
+        bot = new BotActions(hardwareMap);
+        drive = new PinpointDrive(hardwareMap, start);
+        path = createPath();
 
+
+
+
+        while (opModeInInit()){
+            telemetry.addLine("Initializing");
+            telemetry.update();
+        }
+        waitForStart();
+        while (opModeIsActive()){
+            switch (autoStates){
+                case followingPath:
+                    running = path.run(tel);
+                    if(!running) autoStates = AutoStates.idle;
+                    break;
+                case idle:
+                    break;
+            }
+        }
+    }
+    public SequentialAction createPath(){
+        return new SequentialAction(
+                specimens()
+        );
+    }
+
+    public Action specimens(){
+        return drive.actionBuilder(start)
+                .afterTime(0, new ParallelAction(
+                        bot.rClawMIDAction(),
+                        bot.clawCloseAction(),
+                        bot.armMidAction()
+                ))
+                .build();
+    }
 }
