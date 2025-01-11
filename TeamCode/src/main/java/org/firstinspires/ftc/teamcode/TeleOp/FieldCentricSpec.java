0be0fb2b0e9ec93865c247e58521ee5da58c91c1
sparky520 <jonathanfishernj@gmail.com>
2025-01-11 15:07:34 -0600

    boop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricSpec.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldCentricSpec.java
@@ -96,7 +96,7 @@ public class FieldCentricSpec extends OpMode
         if (operator.wasJustPressed(GamepadKeys.Button.B)){
             bot.servoClaw.setClawPosition1();
             bot.servoRClaw.setRClawPosMID();
-            bot.aX.setArmPosSUB();
+            bot.aX.setArmPosMID();
             //bot.servoClaw.setClawPosition0();
             try {
                 Thread.sleep(500);
