027d5fb9c56ef8379c6618f4d9e3d1682578194f
sparky520 <jonathanfishernj@gmail.com>
2025-01-29 13:25:00 -0600

    boop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/color/AutoTSEblue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/color/AutoTSEblue.java
@@ -1,7 +1,24 @@
 package org.firstinspires.ftc.teamcode.color;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.AngularVelConstraint;
+import com.acmerobotics.roadrunner.MinVelConstraint;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.VelConstraint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Auto.Executables.ActionsFolder.BotActions;
+import org.firstinspires.ftc.teamcode.Auto.RRdrives.PinpointDrive;
+
+import java.util.Arrays;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -36,6 +53,7 @@ public class AutoTSEblue extends LinearOpMode {
         });
 
         waitForStart();
+
 
     }
 }
