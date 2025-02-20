8e59b7fdb5b8b651ef4dc5874b5ede96cb6a86b8
Nick-Kwan <144179772+Nick-Kwan@users.noreply.github.com>
2025-02-20 17:16:10 -0600

    ?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Mecanum.java
@@ -38,7 +38,7 @@ public class Mecanum {
     }
 
     public void drive(Gamepad gamepad1) {
-        y = -gamepad1.left_stick_y;
+        y = gamepad1.left_stick_y;
         x = gamepad1.left_stick_x;
         rx = gamepad1.right_stick_x;
 
